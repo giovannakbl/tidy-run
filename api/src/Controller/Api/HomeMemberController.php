6f0a335a88a54d1db3cedc0fecd4ba85f4c96491
giovannakbl <giovanna.kiehn.bertuzzi>
2022-06-23 19:16:01 +0200

    Add Tidy User, Home Member and token generator endpoints

--- api/src/Controller/Api/HomeMemberController.php
+++ api/src/Controller/Api/HomeMemberController.php
@@ -2,51 +2,231 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\HomeMember;
 use App\Repository\HomeMemberRepository;
+use App\Repository\TidyUserRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
 
+/**
+ * @Route("/api/v1/home_members")
+ */
 class HomeMemberController extends AbstractController
 {
+
     /**
-     * @Route("/api/homemembers", methods={"GET"})
+     * @Route("", methods={"GET"})
      */
-    public function listHomeMembers(Request $request, HomeMemberRepository $homeMemberRepository): Response
+    public function listHomeMembers(HomeMemberRepository $homeMemberRepository): Response
     {
-        $tidy_user_id = $request->query->get('tidy_user_id');
-        $homeMembers = $homeMemberRepository->findBy(array('tidy_user' => $tidy_user_id));
-        if ($tidy_user_id == null) {
-            return $this->json([
-                'status_message' => 'Bad Request'
-            ], 400);
-        }
-        $result = [];
-        if ($homeMembers != null) {
-            foreach ($homeMembers as $homeMember) {
-                $result[] = [
-                    'id' => $homeMember->getId(),
-                    'name' => $homeMember->getName(),
-                    'avatar_icon' => $homeMember->getAvatarIcon(),
-                    'icon_color' => $homeMember->getIconColor(),
-                    'deleted_at' => $homeMember->getDeletedAt(),
-                    'tidy_user_id' => $homeMember->getTidyUser()->getId(),
-                    'tidy_user_email' => $homeMember->getTidyUser()->getEmail()
-                ];
-            }
-        } else {
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
             return $this->json([
                 'status_message' => 'User Not Found',
                 'status_code' => 601,
                 'status_name' => 'UserNotFound'
             ], 404);
         }
-        
+        $tidy_user_id = $tidy_user->getId();
+        $homeMembers = $homeMemberRepository->findBy(array('tidy_user' => $tidy_user_id));
+        if ($homeMembers == null) {
+            return $this->json([
+                'home_members' => []
+            ]);
+        }
+        $result = [];
+        foreach ($homeMembers as $homeMember) {
+            if ($homeMember->getDeletedAt() == null) {
+                $result[] = [
+                    'id' => $homeMember->getId(),
+                    'name' => $homeMember->getName(),
+                    'avatar_icon' => $homeMember->getAvatarIcon(),
+                    'icon_color' => $homeMember->getIconColor(),
+                    'deleted_at' => $homeMember->getDeletedAt()
+                ];
+            }
+        }
         return $this->json([
             'home_members' => $result
         ]);
     }
 
+    /**
+     * @Route("", methods={"POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
+            return $this->json([
+                'status_message' => 'User Not Found',
+                'status_code' => 601,
+                'status_name' => 'UserNotFound'
+            ], 404);
+        }
+        $data = $request->toArray();
+        if (isset($data['name']) && isset($data['avatar_icon']) && isset($data['icon_color'])) {
+            try {
+                $homeMember = new HomeMember();
+                $homeMember->setName($data['name']);
+                $homeMember->setAvatarIcon($data['avatar_icon']);
+                $homeMember->setIconColor($data['icon_color']);
+                $homeMember->setTidyUser($tidy_user);
+                $em->persist($homeMember);
+                $em->flush();
+                return $this->json([
+                    'home_member' => [
+                        'id' => $homeMember->getId(),
+                        'name' => $homeMember->getName(),
+                        'avatar_icon' => $homeMember->getAvatarIcon(),
+                        'icon_color' => $homeMember->getIconColor(),
+                        'deleted_at' => $homeMember->getDeletedAt()
+                    ]
+                ]);
+            } catch (\Exception $exception) {
+                return $this->json([
+                    'status_message' => 'Internal Server Error'
+                ], 500);
+            }
+        } else {
+            return $this->json([
+                'status_message' => 'Bad Request'
+            ], 400);
+        }
+    }
+
+
+    /**
+     * @Route("/{id}", methods={"GET"})
+     */
+    public function show($id, HomeMemberRepository $homeMemberRepository): Response
+    {
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
+            return $this->json([
+                'status_message' => 'User Not Found',
+                'status_code' => 601,
+                'status_name' => 'UserNotFound'
+            ], 404);
+        }
+        $homeMember = $homeMemberRepository->find($id);
+        if ($homeMember == null || $homeMember->getTidyUser() != $tidy_user) {
+            return $this->json([
+                'status_message' => 'Home Member Not Found',
+                'status_code' => 602,
+                'status_name' => 'HomeMemberNotFound'
+            ], 404);
+        }
+        if ($homeMember->getDeletedAt() == null) {
+            $home_member_is_active = true;
+        } else {
+            $home_member_is_active = false;
+        }
+        $result = [
+            'id' => $homeMember->getId(),
+            'name' => $homeMember->getName(),
+            'avatar_icon' => $homeMember->getAvatarIcon(),
+            'icon_color' => $homeMember->getIconColor(),
+            'deleted_at' => $homeMember->getDeletedAt()
+        ];
+        return $this->json([
+            'home_member' => $result,
+            'home_member_is_active' => $home_member_is_active
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}", methods={"PUT"})
+     */
+    public function edit($id, HomeMemberRepository $homeMemberRepository, EntityManagerInterface $em, Request $request): Response
+    {
+        $data = $request->toArray();
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
+            return $this->json([
+                'status_message' => 'User Not Found',
+                'status_code' => 601,
+                'status_name' => 'UserNotFound'
+            ], 404);
+        }
+        $homeMember = $homeMemberRepository->find($id);
+        if ($homeMember == null || $homeMember->getTidyUser() != $tidy_user || $homeMember->getDeletedAt() != null) {
+            return $this->json([
+                'status_message' => 'Home Member Not Found',
+                'status_code' => 602,
+                'status_name' => 'HomeMemberNotFound'
+            ], 404);
+        }
+            if (isset($data['name']) || isset($data['avatar_icon']) || isset($data['icon_color'])) {
+                try {
+                    if (isset($data['name'])) {
+                        $homeMember->setName($data['name']);
+                    }
+                    if (isset($data['avatar_icon'])) {
+                        $homeMember->setAvatarIcon($data['avatar_icon']);
+                    }
+                    if (isset($data['icon_color'])) {
+                        $homeMember->setIconColor($data['icon_color']);
+                    }
+                    $em->flush();
+                    return $this->json([
+                        'home_member' => [
+                            'id' => $homeMember->getId(),
+                            'name' => $homeMember->getName(),
+                            'avatar_icon' => $homeMember->getAvatarIcon(),
+                            'icon_color' => $homeMember->getIconColor(),
+                            'deleted_at' => $homeMember->getDeletedAt()
+                        ]
+                    ]);
+                } catch (\Exception $exception) {
+                    return $this->json([
+                        'status_message' => 'Internal Server Error'
+                    ], 500);
+                }
+            
+    } 
+}
+
+    /**
+     * @Route("/{id}", methods={"DELETE"})
+     */
+    public function delete($id, HomeMemberRepository $homeMemberRepository, EntityManagerInterface $em): Response
+    {
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
+            return $this->json([
+                'status_message' => 'User Not Found',
+                'status_code' => 601,
+                'status_name' => 'UserNotFound'
+            ], 404);
+        }
+        $homeMember = $homeMemberRepository->find($id);
+        if ($homeMember == null || $homeMember->getTidyUser() != $tidy_user || $homeMember->getDeletedAt() != null) {
+            return $this->json([
+                'status_message' => 'Home Member Not Found',
+                'status_code' => 602,
+                'status_name' => 'HomeMemberNotFound'
+            ], 404);
+        }
+            try {
+                $current_date = new DateTimeImmutable();
+                $homeMember->setDeletedAt($current_date);
+                $em->flush();
+                return $this->json([
+                    'status_message' => 'Home Member deleted',
+                    'deleted_date' => $current_date
+                ]);
+            } catch (\Exception $exception) {
+                return $this->json([
+                    'status_message' => 'Internal Server Error'
+                ], 500);
+            }
+        
+    }
 }
---
+++ api/src/Controller/Api/TidyUserController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\TidyUser;
+use App\Repository\TidyUserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/api/v1/tidy_user")
+ */
+class TidyUserController extends AbstractController
+{
+    /**
+     * @Route("/register", methods={"POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $data = $request->toArray();
+        if (isset($data['email']) && isset($data['password'])) {
+            try {
+                $tidy_user = new TidyUser();
+                $tidy_user->setEmail($data['email']);
+                $hashedPassword = $passwordHasher->hashPassword($tidy_user, $data['password']);
+                $tidy_user->setPassword($hashedPassword);
+                $tidy_user->setRoles(["ROLE_USER"]);
+                $em->persist($tidy_user);
+                $em->flush();
+                return $this->json([
+                    'tidy_user' => [
+                        'id' => $tidy_user->getId(),
+                        'email' => $tidy_user->getEmail()
+                    ]
+                ]);
+            } catch (\Exception $exception) {
+                if ($exception->getCode() == 1062) {
+                    return $this->json([
+                        'status_message' => 'Email is already registered',
+                        'status_code' => 603,
+                        'status_name' => 'DuplicatedEmail'
+                    ], 400);
+                } else {
+                    return $this->json([
+                        'status_message' => 'Internal Server Error'
+                    ], 500);
+                }
+            }
+        } else {
+            return $this->json([
+                'status_message' => 'Bad Request'
+            ], 400);
+        }
+    }
+
+    /**
+     * @Route("", methods={"GET"})
+     */
+    public function show(): Response
+    {
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
+            return $this->json([
+                'status_message' => 'User Not Found',
+                'status_code' => 601,
+                'status_name' => 'UserNotFound'
+            ], 404);
+        }
+        $result = [
+            'id' => $tidy_user->getId(),
+            'email' => $tidy_user->getEmail(),
+            'home_name' => $tidy_user->getHomeName()
+        ];
+        return $this->json([
+            'tidy_user' => $result
+        ]);
+    }
+
+    /**
+     * @Route("", methods={"PUT"})
+     */
+    public function edit(EntityManagerInterface $em, Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $data = $request->toArray();
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
+            return $this->json([
+                'status_message' => 'User Not Found',
+                'status_code' => 601,
+                'status_name' => 'UserNotFound'
+            ], 404);
+        }
+        if (isset($data['email']) || isset($data['password']) || isset($data['home_name'])) {
+            try {
+                if (isset($data['email'])) {
+                    $tidy_user->setEmail($data['email']);
+                }
+                if (isset($data['password'])) {
+                    $hashedPassword = $passwordHasher->hashPassword($tidy_user, $data['password']);
+                    $tidy_user->setPassword($hashedPassword);
+                }
+                if (isset($data['home_name'])) {
+                    $tidy_user->setHomeName($data['home_name']);
+                }
+                $em->flush();
+                return $this->json([
+                    'tidy_user' => [
+                        'id' => $tidy_user->getId(),
+                        'email' => $tidy_user->getEmail(),
+                        'home_name' => $tidy_user->getHomeName()
+                    ]
+                ]);
+            } catch (\Exception $exception) {
+                if ($exception->getCode() == 1062) {
+                    return $this->json([
+                        'status_message' => 'Email is already registered',
+                        'status_code' => 603,
+                        'status_name' => 'DuplicatedEmail'
+                    ], 400);
+                } else {
+                    return $this->json([
+                        'status_message' => 'Internal Server Error'
+                    ], 500);
+                }
+            }
+        }
+    }
+
+    /**
+     * @Route("", methods={"DELETE"})
+     */
+    public function delete(TidyUserRepository $tidyUserRepository): Response
+    {
+        $tidy_user = $this->getUser();
+        if ($tidy_user == null) {
+            return $this->json([
+                'status_message' => 'User Not Found',
+                'status_code' => 601,
+                'status_name' => 'UserNotFound'
+            ], 404);
+        }
+        try {
+            $tidyUserRepository->remove($tidy_user, true);
+            return $this->json([
+                'status_message' => 'User deleted',
+            ]);
+        } catch (\Exception $exception) {
+            return $this->json([
+                'status_message' => 'Internal Server Error'
+            ], 500);
+        }
+    }
+}
